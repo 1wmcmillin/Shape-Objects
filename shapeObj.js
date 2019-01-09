020ac1102379e2cd30c6d2ab80791f14c7df1b2e
1wmcmillin <42749672+1wmcmillin@users.noreply.github.com>
2019-01-09 14:03:53 -0600

    Add files via upload

---
+++ shapeObj.js
@@ -0,0 +1,34 @@
+//Your code
+
+let Rectangle = {
+    length: 4,
+    width: 10,
+
+    perimeter: function(){
+        let p = 2*this.length + 2*this.width;
+        return p;
+    },
+
+    area: function(){
+        let a = this.length * this.width;
+        return a;
+    }
+    
+}
+
+let Square = {
+
+}
+
+let Circle = {
+
+}
+
+//Dont forget about box and sphere
+
+
+
+
+
+//------------------------------------------------
+//copy paste test code when you finish under here
